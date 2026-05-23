9d5cdbc64c22791ab62a6a6ee194da2a05855d6a
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 09:46:03 +0530

    Add files via upload

---
+++ Player.jsx
@@ -0,0 +1,14 @@
+function Player() {
+
+  return (
+    <div className="player-box">
+
+      <h1>StudyFlix Player 🎥</h1>
+
+      <div className="video-player"></div>
+
+    </div>
+  );
+}
+
+export default Player;
