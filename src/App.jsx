ab99c5669e643989d11f7af6899fef6cae737c8d
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 10:08:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 import Dashboard from "./pages/Dashboard";
 import Player from "./pages/Player";
 
 function App() {
+
   return (
+
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<Login />} />
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/admin" element={<Admin />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/player" element={<Player />} />
+
       </Routes>
+
     </BrowserRouter>
+
   );
 }
 
