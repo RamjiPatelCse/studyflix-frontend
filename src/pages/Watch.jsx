bc007a32d41ee1bb18b33949ef1e4f735c0763d4
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 15:30:33 +0530

    Update Watch.jsx

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,91 +1,39 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
-export default function Watch() {
+export default function Watch(){
+
   const router = useRouter();
-  const { id } = router.query;
 
-  const [videoUrl, setVideoUrl] = useState("");
+  const { url } = router.query;
 
-  useEffect(() => {
-    if (!id) return;
+  if(!url){
 
-    async function loadVideo() {
-      try {
-        const res = await fetch(
-          `https://ppx-d3d205f7.koyeb.app/lecture/${id}`
-        );
+    return <h1>Loading...</h1>;
 
-        const data = await res.json();
-
-        let finalUrl = "";
-
-        // PDF ignore
-        if (data.pdf_link) {
-          console.log("PDF Ignored");
-        }
-
-        // Direct m3u8
-        if (
-          data.path &&
-          data.path.includes(".m3u8")
-        ) {
-          finalUrl = data.path;
-        }
-
-        // Secure Player
-        else if (
-          data.video_player_url &&
-          data.video_player_token
-        ) {
-          finalUrl =
-            data.video_player_url +
-            data.video_player_token;
-        }
-
-        setVideoUrl(finalUrl);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    loadVideo();
-  }, [id]);
-
-  if (!videoUrl) {
-    return (
-      <div
-        style={{
-          background: "#000",
-          color: "#fff",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "30px",
-        }}
-      >
-        Loading Player 😄🔥
-      </div>
-    );
   }
 
-  return (
-    <div
+  return(
+
+    <iframe
+
+      src={decodeURIComponent(url)}
+
+      width="100%"
+
+      height="100%"
+
       style={{
-        background: "#000",
-        height: "100vh",
+        position:"fixed",
+        top:0,
+        left:0,
+        border:"none",
+        background:"#000"
       }}
-    >
-      <iframe
-        src={videoUrl}
-        width="100%"
-        height="100%"
-        allowFullScreen
-        style={{
-          border: "none",
-        }}
-      />
-    </div>
+
+      allowFullScreen
+
+    />
+
   );
+
 }
