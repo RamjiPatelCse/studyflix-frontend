19fdb55896afcd1556f906d5d9a4d85e3a090acc
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 14:54:26 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState }
+from "react";
 
 function Home() {
 
@@ -7,71 +8,88 @@ function Home() {
 
   useEffect(() => {
 
-    loadBatches();
+    const data =
+    JSON.parse(
 
-  }, []);
-
-  const loadBatches =
-  async () => {
-
-    try {
-
-      const res =
-      await fetch(
-
-        "https://studyflix-backend.onrender.com/batches"
-
-      );
-
-      const data =
-      await res.json();
-
-      setBatches(data);
-
-    } catch (err) {
-
-      console.log(err);
-
-    }
-
-  };
-
-  const deleteBatch =
-  async (id) => {
-
-    await fetch(
-
-      `https://studyflix-backend.onrender.com/delete-batch/${id}`,
-
-      {
-        method:"DELETE"
-      }
+      localStorage.getItem(
+        "batches"
+      ) || "[]"
 
     );
 
-    loadBatches();
+    setBatches(data);
 
-  };
+  }, []);
 
   return (
 
     <div
+
       style={{
+
         background:"#000",
+
         minHeight:"100vh",
+
         padding:"20px",
+
         color:"#fff"
+
       }}
+
     >
 
       <h1
+
         style={{
+
           color:"red",
-          marginBottom:"20px"
+
+          marginBottom:"20px",
+
+          fontSize:"35px"
+
         }}
+
       >
+
         StudyFlix 😄🔥
+
       </h1>
+
+
+
+      {
+
+        batches.length === 0 && (
+
+          <div
+
+            style={{
+
+              textAlign:"center",
+
+              marginTop:"100px",
+
+              opacity:"0.7"
+
+            }}
+
+          >
+
+            <h2>
+
+              No Batch Found 😄
+
+            </h2>
+
+          </div>
+
+        )
+
+      }
+
+
 
       {
 
@@ -81,11 +99,41 @@ function Home() {
 
             key={batch.id}
 
+
+
+            onClick={() => {
+
+              localStorage.setItem(
+
+                "selectedBatch",
+
+                JSON.stringify(batch)
+
+              );
+
+
+
+              window.location.href =
+              "/player";
+
+            }}
+
+
+
             style={{
+
               background:"#111",
+
               borderRadius:"20px",
+
               overflow:"hidden",
-              marginBottom:"20px"
+
+              marginBottom:"25px",
+
+              cursor:"pointer",
+
+              border:"2px solid #222"
+
             }}
 
           >
@@ -96,77 +144,39 @@ function Home() {
 
               alt=""
 
+
+
               style={{
+
                 width:"100%",
+
                 height:"220px",
+
                 objectFit:"cover"
+
               }}
 
             />
 
+
+
             <div
+
               style={{
+
                 padding:"15px"
+
               }}
+
             >
 
               <h2>
+
                 {batch.title}
+
               </h2>
 
-              <button
 
-                onClick={() => {
-
-                  localStorage.setItem(
-
-                    "selectedBatch",
-
-                    JSON.stringify(batch)
-
-                  );
-
-                  window.location.href =
-                  "/player";
-
-                }}
-
-                style={{
-                  padding:"12px",
-                  border:"none",
-                  borderRadius:"10px",
-                  background:"red",
-                  color:"#fff",
-                  marginRight:"10px",
-                  cursor:"pointer"
-                }}
-
-              >
-
-                Open
-
-              </button>
-
-              <button
-
-                onClick={() =>
-                  deleteBatch(batch.id)
-                }
-
-                style={{
-                  padding:"12px",
-                  border:"none",
-                  borderRadius:"10px",
-                  background:"#222",
-                  color:"#fff",
-                  cursor:"pointer"
-                }}
-
-              >
-
-                Delete
-
-              </button>
 
             </div>
 
