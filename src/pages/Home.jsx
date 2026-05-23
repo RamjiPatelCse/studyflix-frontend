ce53f4d75b00a9210af7c0c03db23c974cde145b
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 10:56:04 +0530

    Fetch and display batches from API in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
 
-  const [name, setName] = useState("");
+  const [batches, setBatches] = useState([]);
+
+  useEffect(() => {
+
+    getBatches();
+
+  }, []);
+
+  const getBatches = async () => {
+
+    try {
+
+      const response = await axios.get(
+        "https://studyflix-backend.onrender.com/api/batches"
+      );
+
+      setBatches(response.data.batches);
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  };
 
   return (
 
@@ -10,60 +35,53 @@ function Home() {
       style={{
         minHeight:"100vh",
         background:"#111",
-        color:"white",
-        padding:20
+        padding:20,
+        color:"white"
       }}
     >
 
-      <h1 style={{color:"red"}}>
+      <h1
+        style={{
+          color:"red",
+          marginBottom:20
+        }}
+      >
         StudyFlix 😄
       </h1>
 
-      <br/>
-
-      <input
-        placeholder="Enter Your Name"
-        value={name}
-        onChange={(e)=>setName(e.target.value)}
-        style={{
-          padding:14,
-          width:"100%",
-          borderRadius:10,
-          border:"none",
-          marginBottom:20
-        }}
-      />
-
       {
-        name &&
-        <h2>
-          Welcome {name} 👋
-        </h2>
+        batches.map((batch) => (
+
+          <div
+            key={batch.id}
+            style={{
+              background:"#1b1b1b",
+              padding:15,
+              borderRadius:15,
+              marginBottom:20
+            }}
+          >
+
+            <img
+              src={batch.thumbnail}
+              alt=""
+              style={{
+                width:"100%",
+                borderRadius:12,
+                marginBottom:15
+              }}
+            />
+
+            <h2>{batch.batchName}</h2>
+
+            <p>
+              Lectures: {batch.lectures.length}
+            </p>
+
+          </div>
+
+        ))
       }
-
-      <br/>
-
-      <div
-        style={{
-          background:"#1b1b1b",
-          padding:20,
-          borderRadius:14,
-          marginBottom:15
-        }}
-      >
-        <img
-          src="https://i.imgur.com/8Km9tLL.jpeg"
-          style={{
-            width:"100%",
-            borderRadius:12
-          }}
-        />
-
-        <br/><br/>
-
-        <h2>Coding Python</h2>
-
-      </div>
 
     </div>
   );
