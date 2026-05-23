3c0d2a8b6643d3b0c81bec5a114d45984dd6a5cf
RamjiPatelCse <23jdcs045@gmail.com>
2026-05-23 15:29:27 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,94 +2,108 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Home() {
-  const [batches, setBatches] = useState([]);
+
   const router = useRouter();
 
-  useEffect(() => {
-    const data =
-      JSON.parse(localStorage.getItem("studyflix_batches")) || [];
+  const [courses, setCourses] =
+    useState([]);
 
-    setBatches(data);
+  useEffect(() => {
+
+    const data =
+      JSON.parse(
+        localStorage.getItem(
+          "courses"
+        ) || "[]"
+      );
+
+    setCourses(data);
+
   }, []);
 
   return (
+
     <div
       style={{
-        background: "#000",
-        minHeight: "100vh",
-        padding: "20px",
-        color: "#fff",
+        background:"#000",
+        minHeight:"100vh",
+        padding:"20px",
+        color:"#fff"
       }}
     >
-      {/* Logo */}
+
       <h1
         style={{
-          color: "red",
-          fontSize: "60px",
-          marginBottom: "30px",
-          fontWeight: "bold",
+          color:"red",
+          fontSize:"50px",
+          marginBottom:"30px"
         }}
       >
         StudyFlix 😄🔥
       </h1>
 
-      {/* Empty */}
-      {batches.length === 0 && (
-        <div
-          style={{
-            textAlign: "center",
-            marginTop: "100px",
-            fontSize: "30px",
-          }}
-        >
-          No Batch Found 😭
-        </div>
-      )}
+      {
 
-      {/* Batch List */}
-      {batches.map((batch, index) => (
-        <div
-          key={index}
-          onClick={() =>
-            router.push(`/course/${batch.id}`)
-          }
-          style={{
-            background: "#111",
-            borderRadius: "25px",
-            overflow: "hidden",
-            marginBottom: "30px",
-            cursor: "pointer",
-            border: "2px solid #222",
-          }}
-        >
-          {/* Thumbnail */}
-          <img
-            src={batch.thumbnail}
-            alt="thumb"
-            style={{
-              width: "100%",
-              height: "320px",
-              objectFit: "cover",
-            }}
-          />
+        courses.map((course,index)=>(
 
-          {/* Info */}
           <div
+
+            key={index}
+
+            onClick={() =>
+              router.push(
+                `/course/${course.id}`
+              )
+            }
+
             style={{
-              padding: "20px",
+              background:"#111",
+              borderRadius:"20px",
+              overflow:"hidden",
+              marginBottom:"25px",
+              cursor:"pointer"
             }}
+
           >
-            <h2
+
+            <img
+
+              src={course.thumbnail}
+
+              alt=""
+
               style={{
-                fontSize: "38px",
-                lineHeight: "55px",
+                width:"100%",
+                height:"250px",
+                objectFit:"cover"
+              }}
+
+            />
+
+            <div
+              style={{
+                padding:"20px"
               }}
             >
-              {batch.title}
-            </h2>
+
+              <h2
+                style={{
+                  fontSize:"30px"
+                }}
+              >
+                {course.title}
+              </h2>
+
+            </div>
+
           </div>
-        </div>
-      ))}
+
+        ))
+
+      }
+
     </div>
+
   );
+
 }
